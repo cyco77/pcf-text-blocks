6f5220ed917c27a41bf0b8be9121121561809496
Lars Hildebrandt <lars.hildebrandt@tuleva.de>
2023-05-30 19:51:15 +0200

    formated test is now supported

--- PCFTextBlocks/components/textBlock/textBlockComponent.tsx
+++ PCFTextBlocks/components/textBlock/textBlockComponent.tsx
@@ -1,5 +1,6 @@
 import { Icon, PrimaryButton, Stack, TextField } from "@fluentui/react";
 import * as React from "react";
+import copy from "copy-to-clipboard";
 import { ITextBlock } from "../../types/textBlock";
 import { containerStackTokens } from "../common/common";
 
@@ -9,8 +10,7 @@ export interface ITextBlockComponentProps {
 
 const TextBlockComponent: React.FC<ITextBlockComponentProps> = (props: ITextBlockComponentProps) => {
   const copyToClipboard = async () => {
-    await navigator.clipboard.writeText(props.textBlock.value);
-    alert("Kopiert");
+    copy(props.textBlock.value, { format: "text/html" });
   };
 
   return (
@@ -19,8 +19,9 @@ const TextBlockComponent: React.FC<ITextBlockComponentProps> = (props: ITextBloc
         <TextField label='Baustein' value={props.textBlock.name} />
       </Stack>
       <Stack tokens={containerStackTokens}>
-        <div>
-          <TextField multiline autoAdjustHeight readOnly value={props.textBlock.value}></TextField>
+        <div className='textBlockContent'>
+          <div dangerouslySetInnerHTML={{ __html: props.textBlock.value }}></div>
+          {/* <TextField multiline autoAdjustHeight readOnly value={props.textBlock.value}></TextField> */}
           {/* <pre className='preWrap'>{props.textBlock.value}</pre> */}
         </div>
         <div className='copyButton'>
